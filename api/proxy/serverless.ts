37fe698a8c1f5eac8cb4e2e4c5ae76156bf416b5
constantiux <18105757+constantiux@users.noreply.github.com>
2022-12-13 21:35:20 +0800

    add fastify proxy

--- api/proxy/serverless.ts
+++ api/proxy/serverless.ts
@@ -5,11 +5,45 @@ dotenv.config();
 
 import Fastify from "fastify";
 
+const CONFIG = {
+  apiKey: process.env.API_KEY,
+};
+
 const app = Fastify({
   logger: true,
 });
 
 app.register(import("../src"));
+
+app.register(require('@fastify/http-proxy'), {
+  prefix: '/api',
+  upstream: "https://iamshk.glitch.me",
+  undici: true,
+  /**
+   * Reply options to be passed through to `fastify-reply-from`,
+   * which `fastify-http-proxy` uses under the hood.
+   *
+   * @see https://github.com/fastify/fastify-http-proxy#replyoptions
+   */
+  replyOptions: {
+      /**
+       * @see https://github.com/fastify/fastify-reply-from#rewriterequestheadersoriginalreq-headers
+       */
+      rewriteRequestHeaders: (originalRequest, headers) => {
+          return {
+              /**
+               * Preserve the existing request headers.
+               */
+              ...headers,
+              /**
+               * Add the header which the API we're proxying requests
+               * to requires to authenticate the request.
+               */
+              'X-Api-Key': CONFIG.apiKey,
+          };
+      },
+  },
+});
 
 export default async (req, res) => {
   await app.ready();
